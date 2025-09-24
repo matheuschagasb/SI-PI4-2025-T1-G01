d2766be4d061353c765be60a6baa4569e20b9466
Marcos Junior <juninhomazzero@gmail.com>
2025-09-24 20:15:53 -0300

    Create initial structure for backend

---
+++ airane-back/src/test/java/com/pi4/airane_back/AiraneBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi4.airane_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiraneBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
